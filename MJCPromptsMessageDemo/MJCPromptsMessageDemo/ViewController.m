9a599ab9392f7ee1946a5f3b41d8cfc5c9476d36
MJC <292251588@qq.com>
2016-11-23 08:47:55 +0800

    代码修改

--- MJCPromptsMessageDemo/MJCPromptsMessageDemo/ViewController.m
+++ MJCPromptsMessageDemo/MJCPromptsMessageDemo/ViewController.m
@@ -25,49 +25,6 @@
     [super viewDidLoad];
     
     
-//    UIView *view1 = [[UIView alloc]init];
-//    
-//    view1.backgroundColor = [UIColor purpleColor];
-//    
-//    view1.frame = CGRectMake(0, 64, self.view.mjc_width,60);
-//    
-//    [self.view addSubview:view1];
-//    
-//    
-//    // 添加旋转器
-//    UIImageView *imageView = [[UIImageView alloc] init];
-//    imageView.image = [UIImage imageNamed:@"LOL0"];
-//    imageView.mjc_width = 20;
-//    imageView.mjc_height = imageView.mjc_width;
-//    imageView.mjc_x = 10;
-//    //Y值中心点,等于窗口的高度的一半那个位置
-//    CGFloat centerY = view1.mjc_height * 0.5;
-//    imageView.mjc_centerY = centerY;
-//    [view1 addSubview:imageView];
-//
-//    
-//    
-//    // 创建lable
-//    UILabel *label = [[UILabel alloc] init];
-//    label.backgroundColor = [UIColor redColor];
-//    label.font = [UIFont systemFontOfSize:12];
-//    label.textAlignment = NSTextAlignmentLeft;//文字向左对齐
-//    label.numberOfLines = 0;//行数
-//    label.mjc_x =(imageView.mjc_width + 10)+10;
-//    label.mjc_y = 0;
-//    label.mjc_width = view1.mjc_width - (imageView.mjc_width + 10+10+10);
-//    label.mjc_height = view1.mjc_height;
-//    
-//    label.userInteractionEnabled = YES;
-//    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(hideDismiss)];
-//    [label addGestureRecognizer:tapGesture];
-//    label.text = @"阿萨德撒的撒打算打萨达aaaa打算打打算打打算打打算打打算打打算打a打打算打a打算打a打算打a打算打a打算打a打算打a打算打a打算打a打算打a打算打a打算打a打算打a打算打a打算打a打算打a打算打a打打算打a打打算打a";
-//    
-//    [view1 addSubview:label];
-
-    
-
-    
 }
 
 - (IBAction)successOne:(id)sender {
